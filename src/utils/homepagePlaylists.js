a603c7b7b69fc4e6dafe9ca086cd62966f3c64c1
SUBHADIP BERA <161728560+Subha62@users.noreply.github.com>
2026-03-30 00:48:05 +0530

    Update homepagePlaylists.js

--- src/utils/homepagePlaylists.js
+++ src/utils/homepagePlaylists.js
@@ -1,22 +1,47 @@
+// export const homepagePlaylistInfo = [
+//   {
+//     title: "Trending Now",
+//     id: "OLAK5uy_lSTp1DIuzZBUyee3kDsXwPgP25WdfwB40",
+//   },
+//   {
+//     title: "New Music Hindi",
+//     id: "RDCLAK5uy_nNhhgRET3NcJ4SJBvqhAIJ6t7vjsQYowc",
+//   },
+//   {
+//     title: "Bollywood Hits",
+//     id: "RDCLAK5uy_n9Fbdw7e6ap-98_A-8JYBmPv64v-Uaq1g",
+//   },
+//   {
+//     title: "90s Bollywood Romance",
+//     id: "RDCLAK5uy_lnm4v4arFrmL63NUzIdoXJe-E7G4_sriU",
+//   },
+//   {
+//     title: "Motivational Tracks",
+//     id: "RDCLAK5uy_nyta-kQ65CIZc84rEbmNVk7y2oZt4HB1A",
+//   },
+// ];
+
+
+
 export const homepagePlaylistInfo = [
   {
     title: "Trending Now",
-    id: "OLAK5uy_lSTp1DIuzZBUyee3kDsXwPgP25WdfwB40",
+    id: "PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI",
   },
   {
     title: "New Music Hindi",
-    id: "RDCLAK5uy_nNhhgRET3NcJ4SJBvqhAIJ6t7vjsQYowc",
+    id: "PL4fGSI1pDJn5rWitrRWFKdm-ulaFiIyoK",
   },
   {
     title: "Bollywood Hits",
-    id: "RDCLAK5uy_n9Fbdw7e6ap-98_A-8JYBmPv64v-Uaq1g",
+    id: "PLgzTt0k8mXzEk586ze4BjvDXR7c-TUSnx",
   },
   {
     title: "90s Bollywood Romance",
-    id: "RDCLAK5uy_lnm4v4arFrmL63NUzIdoXJe-E7G4_sriU",
+    id: "PL9bw4S5ePsEHr6T5s0Xh2iL0p8nH3UX0D",
   },
   {
     title: "Motivational Tracks",
-    id: "RDCLAK5uy_nyta-kQ65CIZc84rEbmNVk7y2oZt4HB1A",
+    id: "PLARpUvpFxzhbLTR9QN5bBcXTin3vUvlPO",
   },
 ];
